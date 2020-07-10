921e890a153fbb5a20f775b277ce2123fac0b9db
Nicolas Racicot <racicot.nicolas2@gmail.com>
2020-07-09 20:35:15 -0400

    Organisation in order to start project

--- RPGLearning/RPGLearning.Designer.cs
+++ RPGLearning/RPGLearning.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace RPGLearning
 {
-    partial class Form1
+    partial class RPGLearning
     {
         /// <summary>
         /// Variable nécessaire au concepteur.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // RPGLearning
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(713, 634);
+            this.Name = "RPGLearning";
+            this.Text = "RPGLearning";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- RPGLearning/RPGLearning.cs
+++ RPGLearning/RPGLearning.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace RPGLearning
 {
-    public partial class Form1 : Form
+    public partial class RPGLearning : Form
     {
-        public Form1()
+        public RPGLearning()
         {
             InitializeComponent();
         }
